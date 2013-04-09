9507b402f819231f0d21a9a45f29ad81a59a7d9c
tangqiwen <tqw4321@126.com>
2013-04-09 11:55:36 +0800

    add AndEngineMultiplayerExtension

---
+++ AndEngineMultiplayerExtension/gen/org/andengine/extension/multiplayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.andengine.extension.multiplayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
